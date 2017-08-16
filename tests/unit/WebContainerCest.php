8ee897667cbe72d77e3c0a7ad172b979aff83d6b
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2017-08-16 15:57:03 +0530

    updating code

---
+++ tests/unit/WebContainerCest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class WebContainerCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    public function _after(UnitTester $I)
+    {
+    }
+
+    public function checkContainerIsRunning(UnitTester $I){
+        $I->wantTo("verify ubuntu container up and running");
+        $I->runShellCommand("docker inspect -f {{.State.Running}} uat_web");
+        $I->seeInShellOutput("true");
+    }
+
+
+    public function checkPHPVersion(UnitTester $I){
+        $I->wantTo("verify php 5.5 is installed in the container");
+        $I->runShellCommand("docker exec uat_web php --version");
+        $I->seeInShellOutput('PHP 5.5');
+    }
+
+    public function checkPHPUnitVersion(UnitTester $I){
+        $I->wantTo("verify phpunit library is installed in the container");
+        $I->runShellCommand("docker exec dev_web phpunit --version");
+        $I->seeInShellOutput('PHPUnit 4.8.34');
+    }
+
+
+
+    public function checkApacheServiceIsRunning(UnitTester $I){
+        $I->wantTo("verify apache is up and running in the container");
+        $I->runShellCommand("ping -c 10 localhost");
+        $I->runShellCommand("docker exec uat_web service apache2 status");
+        $I->seeInShellOutput('apache2 is running');
+    }
+
+
+}
