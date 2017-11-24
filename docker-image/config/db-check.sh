42697454739d0357c0e61ca51a82d2527a046fce
thulana <thulanakannangara@gmail.com>
2017-11-24 12:09:28 +0530

    fix db check

--- docker-image/config/db-check.sh
+++ docker-image/config/db-check.sh
@@ -1,6 +1,16 @@
-until nmap -P0 -sT -p3306 dbserver
-do
-  echo "Waiting for database connection..."
-  # wait for 5 seconds before check again
-  sleep 5
+#until nmap -P0 -sT –open -p3306 dbserver
+#do
+#  echo "Waiting for database connection..."
+#  # wait for 5 seconds before check again
+#  sleep 5
+#done
+while true; do
+ NMAP=$(nmap -P0 -sT -p3306 dbserver)
+ if [[ $NMAP == *"Host is up"* || $NMAP == *"closed"* ]]; then
+    echo "connection!!!"
+    break
+ fi
+
+ sleep 5
+ echo "waiting for connection"
 done
