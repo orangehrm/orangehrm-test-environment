f91dac4e2700c480f2aed64ccb25b32bb1627b56
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-22 10:43:06 +0530

    updated test php version test

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -19,9 +19,9 @@ class WebContainerCest
 
 
     public function checkPHPVersion(UnitTester $I){
-        $I->wantTo("verify php 7.0 is installed in the container");
+        $I->wantTo("verify php 5.6 is installed in the container");
         $I->runShellCommand("docker exec test_web php --version");
-        $I->seeInShellOutput('PHP 7.0');
+        $I->seeInShellOutput('PHP 5.6');
     }
 
     public function checkPHPUnitVersion(UnitTester $I){
@@ -41,17 +41,5 @@ class WebContainerCest
         $I->runShellCommand("docker exec test_web bash -c 'yum info php-pecl-xdebug | grep Version'");
         $I->seeInShellOutput('Version');
         $I->seeInShellOutput('2.');
-    }
-
-    public function checkAstIsInstalled(UnitTester $I){
-        $I->wantTo("verify ast module is installed in the image");
-        $I->runShellCommand("docker exec test_web php -m");
-        $I->seeInShellOutput('ast');
-    }
-
-    public function checkStatsIsInstalled(UnitTester $I){
-        $I->wantTo("verify stats module is installed in the image");
-        $I->runShellCommand("docker exec test_web php -m");
-        $I->seeInShellOutput('stats');
     }
 }
