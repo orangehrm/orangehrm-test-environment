e05f95ed28524535f233f69a6109517b7a8a78a5
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-22 13:08:29 +0530

    added svn and sendmail, vim installation and test cases

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -49,10 +49,73 @@ class WebContainerCest
         $I->seeInShellOutput("3.10.10");
     }
 
-    public function nodeJSTest(UnitTester $I){
-        $I->wantTo("verify availability of node js");
+    public function checkGitVersion(AcceptanceTester $I){
+        $I->wantTo("verify git is installed in the image");
+        $I->runShellCommand("docker exec test_web git --version");
+        $I->seeInShellOutput('version 1');
+    }
+
+
+    public function checkNMAPIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify nmap is installed in the image");
+        $I->runShellCommand("docker exec test_web nmap -V");
+        $I->seeInShellOutput('version 6');
+    }
+
+
+    public function checkNodeIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify node is installed in the image");
         $I->runShellCommand("docker exec test_web node -v");
-        $I->seeInShellOutput("v6");
+        $I->seeInShellOutput('v6');
+    }
+
+
+    public function checkSendMailIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify sendmail is installed in the image");
+        $I->runShellCommand("docker exec test_web which sendmail");
+        $I->seeInShellOutput('/usr/sbin/sendmail');
+    }
+
+    public function checkSVNIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify SVN is installed in the image");
+        $I->runShellCommand("docker exec test_web svn --version");
+        $I->seeInShellOutput('version 1');
+    }
+
+    public function checkBowerIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify bower is installed in the image");
+        $I->runShellCommand("docker exec test_web bower --version");
+        $I->seeInShellOutput('1.8');
+    }
+
+    public function checkGulpIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify Gulp is installed in the image");
+        $I->runShellCommand("docker exec test_web gulp --version");
+        $I->seeInShellOutput('version: 2');
+    }
+
+    public function checkASTIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify ast module is installed in the image");
+        $I->runShellCommand("docker exec test_web php -m | grep ast");
+        $I->seeInShellOutput('ast');
+    }
+
+    public function checkStatsIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify stats module is installed in the image");
+        $I->runShellCommand("docker exec test_web php -m | grep stats");
+        $I->seeInShellOutput('stats');
+    }
+
+    public function checkVIMIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify vim editor is installed in the image");
+        $I->runShellCommand("docker exec test_web vim --version");
+        $I->seeInShellOutput('Vi IMproved 7');
+    }
+
+    public function checkComposerIsInstalled(AcceptanceTester $I){
+        $I->wantTo("verify composer is installed in the image");
+        $I->runShellCommand("docker exec test_web composer --version");
+        $I->seeInShellOutput('Composer version 1');
     }
 
 }
