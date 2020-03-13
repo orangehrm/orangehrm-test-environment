879cf7cb3436c67bf569a187c396b30dc33d8d79
chanakar <bmcrathnayaka@gmail.com>
2020-03-13 10:13:40 +0530

    updated required fields to support php 7.3 environment

--- tests/unit/WebContainerCest.php
+++ tests/unit/WebContainerCest.php
@@ -19,9 +19,9 @@ class WebContainerCest
 
 
     public function checkPHPVersion(UnitTester $I){
-        $I->wantTo("verify php 5.6 is installed in the container");
+        $I->wantTo("verify php 7.3 is installed in the container");
         $I->runShellCommand("docker exec test_web php --version");
-        $I->seeInShellOutput('PHP 7.2');
+        $I->seeInShellOutput('PHP 7.3');
     }
 
     public function checkPHPUnitVersion(UnitTester $I){
